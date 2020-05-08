7349331f3c2216c75a9b0bd9a8fd1d21329d19ff
nerbyk <danil.maximov2000@gmail.com>
2020-05-08 04:20:52 +0200

    database initialization

--- bot.rb
+++ bot.rb
@@ -6,6 +6,9 @@ require 'dotenv'
 
 Dotenv.load('./.env')
 
+require './db/db.rb'
+db = DB.new
+
 Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   bot.listen do |message|
     if message.from.id == ENV['USER_ID'].to_i
@@ -13,10 +16,10 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
         case message
         when Telegram::Bot::Types::CallbackQuery
           message_button = MessageButton.new
-          message_button.call(bot: bot, message: message, user_input: nil, db: db)
+          message_button.call(bot: bot, message: message, db: db)
         else
           message_responder = MessageResponder.new
-          message_responder.call(bot: bot, message: message, user_input: message.text, db: db)
+          message_responder.call(bot: bot, message: message, db: db, user_input: message.text)
         end
       rescue StandardError => e
         bot.api.send_message(chat_id: message.from.id, text: "Error: #{e}")
