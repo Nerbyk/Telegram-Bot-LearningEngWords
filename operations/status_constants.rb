c4f90982fa3f26f63a937aa9afeff85b421505fb
nerbyk <danil.maximov2000@gmail.com>
2020-05-08 05:09:38 +0200

    prepare to learn status added

--- operations/status_constants.rb
+++ operations/status_constants.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Status
-  UNLEARNED       = 'not learned'
-  IN_LEARNING_LV1 = 'in learning, level 1'
-  IN_LEARNING_LV2 = 'in learning, level 2'
-  IN_LEARNING_LV3 = 'in learning, level 3'
-  LEARNED         = 'learned'
+  UNLEARNED          = 'not learned'
+  PREPARING_TO_LEARN = 'prepare to learn'
+  IN_LEARNING_LV1    = 'in learning, level 1'
+  IN_LEARNING_LV2    = 'in learning, level 2'
+  IN_LEARNING_LV3    = 'in learning, level 3'
+  LEARNED            = 'learned'
 
   def get_status
-    [UNLEARNED, IN_LEARNING_LV1, IN_LEARNING_LV2, IN_LEARNING_LV3, LEARNED]
+    [UNLEARNED, PREPARING_TO_LEARN, IN_LEARNING_LV1, IN_LEARNING_LV2, IN_LEARNING_LV3, LEARNED]
   end
 end
