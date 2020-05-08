8684835a9794f6185c9b3760ef3d99baf07dec4d
nerbyk <danil.maximov2000@gmail.com>
2020-05-08 04:20:31 +0200

    parse spreadsheet method

---
+++ db/db.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'sqlite3'
+require 'sequel'
+require './db/spreadsheet/spreadsheet_parser.rb'
+require './operations/status_constants.rb'
+
+class DB
+  attr_reader :db, :dataset, :table
+  def initialize
+    @db = Sequel.sqlite('my_dictionary.db')
+    @table = :words_list
+    @dataset = create
+  end
+
+  def create
+    db.create_table? table do
+      String :eng
+      String :ru
+      String :status
+    end
+    dataset = db[table]
+  end
+
+  def parse_spreadsheet
+    array_of_new_words = Spreadsheet.new.get_words
+    array_of_old_words = dataset.select_map(%i[eng ru])
+    array_of_new_words -= array_of_old_words unless array_of_old_words.nil?
+    array_of_new_words.each do |row|
+      dataset.insert(eng: row[0], ru: row[1], status: Status::UNLEARNED)
+    end
+  end
+end
