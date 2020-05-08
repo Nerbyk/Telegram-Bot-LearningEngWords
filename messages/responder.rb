3b5422e5dd1d6eb24f0a00b9398b2f584a9a9563
nerbyk <danil.maximov2000@gmail.com>
2020-05-08 04:19:54 +0200

    first command was added(/update_db)

---
+++ messages/responder.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class MessageResponder
+  attr_reader :bot, :message, :db, :user_input
+  def call(bot:, message:, db:, user_input:)
+    @bot = bot
+    @message = message
+    @user_input = user_input
+    @db         = db
+    @id         = ENV['USER_ID'].to_i
+    respond
+  end
+
+  def respond
+    case user_input
+    when '/start'
+      main_menu
+    when '/new'
+      start_learning
+    when '/repeat'
+      repeat_learned
+    when '/update_db'
+      update_db
+    when '/progress'
+      show_progress
+    end
+  end
+
+  def main_menu
+    send_message('Choose right option:\n/new - start learning new words\n/repeat - repeat already learned words\n/update_db - update database by adding new words from spreadseet\n/progress - get learning progress message')
+  end
+
+  def update_db
+    db.parse_spreadsheet
+  end
+
+  def send_message(text)
+    bot.api.send_message(user_id: id, text: text)
+  end
+end
