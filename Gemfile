465d02377fe36b796f7e3515650c17f3b1dd15f1
nerbyk <danil.maximov2000@gmail.com>
2020-05-08 03:00:36 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'sqlite', '~> 1.0', '>= 1.0.2'
+gem 'dotenv', '~> 2.1', '>= 2.1.1'
+gem 'google_drive', '~> 2.1', '>= 2.1.5'
+gem 'rubocop', '~> 0.82.0'
+gem 'sequel', '~> 5.31'
+gem 'telegram-bot-ruby', '~> 0.12.0'
---
+++ bot.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'logger'
+require 'telegram/bot'
+require 'dotenv'
+
+Dotenv.load('./.env')
+
+Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
+  bot.listen do |message|
+    if message.from.id == ENV['USER_ID'].to_i
+      begin
+        case message
+        when Telegram::Bot::Types::CallbackQuery
+          message_button = MessageButton.new
+          message_button.call(bot: bot, message: message, user_input: nil, db: db)
+        else
+          message_responder = MessageResponder.new
+          message_responder.call(bot: bot, message: message, user_input: message.text, db: db)
+        end
+      rescue StandardError => e
+        bot.api.send_message(chat_id: message.from.id, text: "Error: #{e}")
+        # prs_log.error "User_id #{message.from.id}, User name = #{message.from.username} Error = #{e}"
+        p e
+      end
+    else
+      bot.api.send_message(chat_id: message.from.id, text: 'Sorry, but I am a private bot, for more information u can write to my owner - @nerby1')
+    end
+  end
+end
