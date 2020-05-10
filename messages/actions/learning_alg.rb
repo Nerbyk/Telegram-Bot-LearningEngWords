d25a65ae131c8616b61a04226726013dcf243c0a
nerbyk <danil.maximov2000@gmail.com>
2020-05-10 21:43:29 +0200

    lesson words demonstation implemented

--- messages/actions/learning_alg.rb
+++ messages/actions/learning_alg.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
-class Learning < MessageResponder
-  attr_reader :bot, :message, :db, :user_input, :id
-  def call(bot:, message:, db:, user_input:)
-    super(bot, message, user_input, db, id)
+require 'singleton'
+
+class Learning
+  include Singleton
+  attr_accessor :bot_options, :status, :words
+  def initialize
+    @bot_options = BotOptions.instance
+    @status      = status
+    @words       = words
   end
 
-  def menu(words)
-    # TODO: menu items -
-    # 1. Start lessond
-    # 2. Look at list
-    # 3. Return to menu
+  def menu
+    bot_options.send_message("Enter:\n /start_lesson - To start lesson\n /look_at_list - To look at the list of words in this lesson\n /start - To return to main menu and to reset list of words")
+  end
+
+  def start_lesson
+    puts('LEsson started')
+  end
+
+  def look_at_list
+    demonstrate_string = ''
+    words.each do |row|
+      demonstrate_string += row[0] + ' - ' + row[1] + "\n"
+    end
+    bot_options.send_message(demonstrate_string)
   end
 end
