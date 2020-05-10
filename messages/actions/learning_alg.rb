a7c5a7ec3c6c1c8794158dcf62af8c06ba54c108
nerbyk <danil.maximov2000@gmail.com>
2020-05-10 21:58:36 +0200

    callback strings added to loo_at_list

--- messages/actions/learning_alg.rb
+++ messages/actions/learning_alg.rb
@@ -12,7 +12,7 @@ class Learning
   end
 
   def menu
-    bot_options.send_message("Enter:\n /start_lesson - To start lesson\n /look_at_list - To look at the list of words in this lesson\n /start - To return to main menu and to reset list of words")
+    bot_options.send_message("Enter:\n /start_lesson - To start lesson\n /look_at_list - To look at the list of words in this lesson\n /reset_lesson - To return to main menu and to reset list of words")
   end
 
   def start_lesson
@@ -24,6 +24,7 @@ class Learning
     words.each do |row|
       demonstrate_string += row[0] + ' - ' + row[1] + "\n"
     end
+    demonstrate_string += "----------\n /start_lesson - To start lesson\n /reset_lesson - To return to main menu and to reset list of words"
     bot_options.send_message(demonstrate_string)
   end
 end
