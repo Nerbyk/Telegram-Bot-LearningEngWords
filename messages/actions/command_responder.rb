b4dbc7c0c125f146027d804688ba9834175fd4c4
nerbyk <danil.maximov2000@gmail.com>
2020-05-10 04:24:08 +0200

    Command design patter implemetation

---
+++ messages/actions/command_responder.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+class Invoker
+  attr_reader :history
+
+  def execute(cmd)
+    @history ||= []
+    @history << cmd.execute
+  end
+end
+
+class Command
+  attr_reader :request
+
+  def initialize(request)
+    @request = request
+  end
+
+  def execute
+    raise NotImplementedError
+  end
+end
+
+class StartCommand < Command
+  def execute
+    request.start
+  end
+end
+
+class NewCommand < Command
+  def execute
+    request.new_list
+  end
+end
+
+class RepeatCommand < Command
+  def execute
+    request.repeat
+  end
+end
+
+class UpdateDbCommand < Command
+  def execute
+    request.update_db
+  end
+end
+
+class ProgressCommand
+  def execute
+    request.progress
+  end
+end
+
+class Receiver
+  attr_reader :bot, :message, :db, :id
+  def initialize(bot:, message:, db:, id:)
+    @bot = bot
+    @message = message
+    @db = db
+    @id = id
+  end
+
+  def start
+    send_message("Choose right option:\n/new - start learning new words\n/repeat - repeat already learned words\n/update_db - update database by adding new words from spreadseet\n/progress - get learning progress message")
+  end
+
+  def new_list
+    send_message('new list command')
+  end
+
+  def repeat
+    send_message('repeat message entered')
+  end
+
+  def update_db
+    db.parse_spreadsheet
+    send_message('database updated, new words were uploaded.')
+    start
+  end
+
+  def progress
+    send_message('progress command entered')
+  end
+
+  def send_message(text)
+    bot.api.send_message(chat_id: id, text: text)
+  end
+end
+
+class AnswerClient
+  attr_reader :invoker, :bot, :message, :db, :id
+  def initialize(bot:, message:, db:, id:)
+    @bot        = bot
+    @message    = message
+    @db         = db
+    @id         = id
+    @receiver = Receiver.new(bot: bot, message: message, db: db, id: id)
+    @invoker = Invoker.new
+  end
+
+  def respond_to(cmd)
+    case cmd
+    when '/start' then @invoker.execute(StartCommand.new(@receiver))
+    when '/new' then @invoker.execute(NewCommand.new(@receiver))
+    when '/repeat' then @invoker.execute(RepeatCommand.new(@receiver))
+    when '/update_db' then @invoker.execute(UpdateDbCommand.new(@receiver))
+    when '/progress' then @invoker.execute(ProgressCommand.new(@receiver))
+    end
+  end
+end
--- messages/responder.rb
+++ messages/responder.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require 'dotenv'
+require './messages/actions/command_responder.rb'
 Dotenv.load('./.env')
 
 class MessageResponder
   attr_reader :bot, :message, :db, :user_input, :id
   def call(bot:, message:, db:, user_input:)
-    @bot = bot
-    @message = message
+    @bot        = bot
+    @message    = message
     @user_input = user_input
     @db         = db
     @id         = ENV['USER_ID'].to_i
@@ -15,29 +16,9 @@ class MessageResponder
   end
 
   def respond
-    case user_input
-    when '/start'
-      main_menu
-    when '/new'
-      start_learning
-    when '/repeat'
-      repeat_learned
-    when '/update_db'
-      update_db
-    when '/progress'
-      show_progress
-    end
+    client = AnswerClient.new(bot: bot, message: message, db: db, id: id)
+    client.respond_to(user_input)
   end
-
-  def main_menu
-    send_message("Choose right option:\n/new - start learning new words\n/repeat - repeat already learned words\n/update_db - update database by adding new words from spreadseet\n/progress - get learning progress message")
-  end
-
-  def update_db
-    db.parse_spreadsheet
-  end
-
-  def start_learning; end
 
   def send_message(text)
     bot.api.send_message(chat_id: id, text: text)
