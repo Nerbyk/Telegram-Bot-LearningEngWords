a3e6fcc492b27ea6bda9f262991384d7a1f16061
nerbyk <danil.maximov2000@gmail.com>
2020-05-10 05:16:48 +0200

    :db initialization mooved to singleton class

--- messages/responder.rb
+++ messages/responder.rb
@@ -5,22 +5,17 @@ require './messages/actions/command_responder.rb'
 Dotenv.load('./.env')
 
 class MessageResponder
-  attr_reader :bot, :message, :db, :user_input, :id
-  def call(bot:, message:, db:, user_input:)
+  attr_reader :bot, :message, :user_input, :id
+  def call(bot:, message:, user_input:)
     @bot        = bot
     @message    = message
     @user_input = user_input
-    @db         = db
     @id         = ENV['USER_ID'].to_i
     respond
   end
 
   def respond
-    client = AnswerClient.new(bot: bot, message: message, db: db, id: id)
+    client = AnswerClient.new
     client.respond_to(user_input)
-  end
-
-  def send_message(text)
-    bot.api.send_message(chat_id: id, text: text)
   end
 end
